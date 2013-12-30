fb15ebf501296aa8eade446aee6ad7f19ac993ba
Chris Schalk <cschalk@themaskedcrusader.com>
2013-12-30 09:39:41 -0700

    Remove update notice

--- src/com/themaskedcrusader/randomchests/Plugin.java
+++ src/com/themaskedcrusader/randomchests/Plugin.java
@@ -16,43 +16,27 @@
 
 package com.themaskedcrusader.randomchests;
 
-import com.themaskedcrusader.bukkit.Library;
 import com.themaskedcrusader.bukkit.config.Settings;
 import com.themaskedcrusader.randomchests.command.CommandListener;
 import com.themaskedcrusader.randomchests.data.KitChests;
 import com.themaskedcrusader.randomchests.data.RandomChests;
 import com.themaskedcrusader.randomchests.listener.ChestListener;
-import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.io.File;
-import java.util.logging.Level;
-
 public class Plugin extends JavaPlugin {
 
     public void onEnable() {
-        try{
-            loadDataFromDisk();
-            new ChestListener(this);
-            getLogger().info("TMCs Random Chests Activated!");
-        } catch (NoClassDefFoundError e) {
-            getLogger().log(Level.SEVERE,  "TMC-LIB Library Missing or cannot load: Disabling Plugin.");
-            getLogger().log(Level.SEVERE,  "See install instructions at http://dev.bukkit.org/server-mods/tmc-lib/");
-            getServer().getPluginManager().disablePlugin(this);
-        }
+        loadDataFromDisk();
+        new ChestListener(this);
+        getLogger().info("TMCs Random Chests Activated!");
     }
 
     private void loadDataFromDisk() {
         Settings.init(this);
         KitChests.loadKitsFromDisk(this);
         RandomChests.loadChestsFromDisk(this);
-        if (Settings.getConfig().getBoolean("check-for-updates")) {
-            Library.checkForNewVersion(getServer().getConsoleSender());
-        } else {
-            getServer().getConsoleSender().sendMessage(ChatColor.AQUA + "Update Check Disabled: See http://dev.bukkit.org/server-mods/tmc-lib/ for updates");
-        }
     }
 
     public void onDisable() {
